cce621ee6cc11ffacee46206127fe9526ff6a2b4
Rach Gee <Rach-Gee@hotmail.com>
2021-10-28 20:34:14 +0800

    create folder stucture, server.js and Trips model

---
+++ server/models/Trips.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const tripsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    require: true,
+    default: false,
+  }
+});
+
+const Trips = model('Trips', tripsSchema);
+
+module.exports = Trips;

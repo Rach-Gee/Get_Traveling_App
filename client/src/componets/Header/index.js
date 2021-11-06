6c4479a17612869086791741a365485bafe7aff1
Rach Gee <Rach-Gee@hotmail.com>
2021-11-06 19:33:39 +0800

    changing header

--- client/src/componets/Header/index.js
+++ client/src/componets/Header/index.js
@@ -1,32 +1,21 @@
 import React from "react";
-import { Heading, Box } from "@chakra-ui/react"
-
-
+import { Box, Stack, Image } from "@chakra-ui/react"
+import plane from '../images/plane.png'
 
 const Header = () => (
   <header>
-    <Box 
-        w="100%"
-        h="200px"
-        bgGradient={[
-          "linear(to-tr, teal.300, yellow.400)",
-          "linear(to-t, blue.200, teal.500)",
-          "linear(to-b, orange.100, purple.300)",
-        ]}
-     >
-      <Heading 
-        size="lg" 
-        fontSize="50px" 
-        pt='50'
-        color="tomato" 
-        textAlign="center"> 
-        The World is Colourful When You Travel 
-      </Heading>
-    </Box>
-
-
-
-  <br />
+    <Stack 
+      spacing={5} 
+      direction="row">
+        <Image src={plane} 
+        alt="world map"
+        />
+      <Box
+      >
+      <br /> <br /> <br /> <br /> <br /> <br /> 
+      <p className="headerFont">~ not all who wander are lost </p>
+      </Box>
+    </Stack>
   </header>
 );
 
